94e5c6664b208d1f1342c2ccdc8f8807775a17d3
Abhilash Khare <abhilashkhare@gmail.com>
2018-08-01 01:32:05 -0700

    feat : Build URL using the GET Response

--- AlamoFireFlickrApp/Controller/ViewController.swift
+++ AlamoFireFlickrApp/Controller/ViewController.swift
@@ -12,6 +12,12 @@ import Alamofire
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    var farmID : AnyObject!
+    var serverID : AnyObject!
+    var ID : AnyObject!
+    var secret : AnyObject!
+    var url : String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,7 +29,26 @@ class ViewController: UIViewController {
                 print(error)
             }
             else{
-                print(result)
+                print(result!)
+                if let farmid = result!["farm"] {
+                    self.farmID = farmid
+                    print(self.farmID!)
+                }
+                if let serverid = result!["server"] {
+                    self.serverID = serverid
+                    print(self.serverID!)
+                }
+                if let id = result!["id"] {
+                    self.ID = id
+                    print(self.ID!)
+                }
+                if let secretID = result!["secret"] {
+                    self.secret = secretID
+                    print(self.secret!)
+                }
+                
+              self.url = "https://farm\(self.farmID!).staticflickr.com/\(self.serverID!)/\(self.ID!)_\(self.secret!).jpg"
+                print(self.url!)
                 
             }
         }
--- AlamoFireFlickrApp/Model/AFFlickrClient.swift
+++ AlamoFireFlickrApp/Model/AFFlickrClient.swift
@@ -13,13 +13,13 @@ import Alamofire
 class AFFlickrClient {
     static var sharedInstance = AFFlickrClient()
     
-    fileprivate func GETImage(_ response: (DataResponse<Any>), _ completionHandlerForGETFetch: @escaping (_ result : AnyObject?, _ error : String?) ->Void)  {
-        if let result = response.result.value as? [String : Any] {
-            if let photos = result["photos"] as? [String : Any]{
-                if let photo = photos["photo"] as? [[String : Any]]{
+    fileprivate func GETImage(_ response: (DataResponse<Any>), _ completionHandlerForGETFetch: @escaping (_ result : [String : AnyObject]?, _ error : String?) ->Void)  {
+        if let result = response.result.value as? [String : AnyObject] {
+            if let photos = result["photos"] as? [String : AnyObject]{
+                if let photo = photos["photo"] as? [[String : AnyObject]]{
                     let randomImage  = Int(arc4random_uniform(UInt32(100)))
-                    if let image = photo[randomImage] as? [String : Any]{
-                        completionHandlerForGETFetch(image as AnyObject,nil)
+                    if let image = photo[randomImage] as? [String : AnyObject]{
+                        completionHandlerForGETFetch(image as? [String : AnyObject],nil)
                     }
                 }
             }
@@ -31,9 +31,8 @@ class AFFlickrClient {
         
     }
     
-    func fetchImageGET(_ completionHandlerForGET: @escaping (_ result : AnyObject?, _ error : String?) ->Void){
+    func fetchImageGET(_ completionHandlerForGET: @escaping (_ result : [String : AnyObject]?, _ error : String?) ->Void){
         let randomPageNumber = arc4random_uniform(UInt32(1000))
-        print(randomPageNumber)
         Alamofire.request("https://api.flickr.com/services/rest/", method: .get, parameters: ["method":"flickr.photos.search","api_key":"c4deaa1b0a7b77672f46c6a4b145eccc", "text":"ocean","page":randomPageNumber,"format":"json","nojsoncallback":"1"]).responseJSON { (response) in
             if response.error != nil{
                 completionHandlerForGET(nil,response.error.debugDescription)
@@ -43,7 +42,7 @@ class AFFlickrClient {
                     if error != nil{
                         completionHandlerForGET(nil,"Error fetching random image")
                     } else {
-                        completionHandlerForGET(result as AnyObject,nil)
+                        completionHandlerForGET(result as? [String : AnyObject],nil)
                     }
                 })
         }
